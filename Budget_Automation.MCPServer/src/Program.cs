889a560d4d53e34ab93487c91f9f122f4abde051
Aitor Bermejo Ruiz <aitorbermeruiz@gmail.com>
2025-05-18 17:43:36 +0200

    Add common read Write Methods

--- Budget_Automation.MCPServer/src/Program.cs
+++ Budget_Automation.MCPServer/src/Program.cs
@@ -1,26 +1,26 @@
-﻿using System.ComponentModel;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ModelContextProtocol.AspNetCore;
-using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.Builder;
-using ModelContextProtocol.Protocol.Types;
-using ModelContextProtocol.Server;
 using Microsoft.AspNetCore.Hosting;
 using Budget_Automation.MCPServer.MCP.Tools;
+using Budget_Automation.MCPServer.Services.Google.Models;
+using Budget_Automation.MCPServer.Services.Google;
+using Budget_Automation.MCPServer.Services.Google.Abstract;
 
 
 var builder = Host.CreateEmptyApplicationBuilder(settings: null);
 
 // Configurar servicios de Google Sheets
-// builder.Services.AddSingleton<IGoogleSheetsService, GoogleSheetsService>();
-// builder.Services.AddSingleton<ISheetConfigurationService, SheetConfigurationService>();
+builder.Services.AddSingleton<GoogleAuthService>(); // Registrar GoogleAuthService
+builder.Services.AddSingleton<IGoogleSheetsService, GoogleSheetsService>();
 
 // Configurar MCP Server con API fluida
 builder.Services
     .AddMcpServer()
     .WithStdioServerTransport()
     .WithTools<WeatherTool>(); // Registramos todas las herramientas del ensamblado
+
+builder.Services.Configure<GoogleApiSettings>(builder.Configuration.GetSection("GoogleApiSettings"));
+
 
 builder.Services.AddHttpClient();
 
--- Budget_Automation.MCPServer/src/Services/Google/GoogleAuthService.cs
+++ Budget_Automation.MCPServer/src/Services/Google/GoogleAuthService.cs
@@ -1,90 +1,36 @@
 
-// using Google.Apis.Auth.OAuth2;
-// using Google.Apis.Services;
-// using Google.Apis.Sheets.v4;
-// using Google.Apis.Util.Store;
-// using Budget_Automation.MCPServer.Services.Google.Abstract;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Util.Store;
+using Budget_Automation.MCPServer.Services.Google.Abstract;
+using Budget_Automation.MCPServer.Services.Google.Models;
+using Microsoft.Extensions.Options;
 
-// namespace Budget_Automation.MCPServer.Services.Google
-// {
-//     public class GoogleAuthService : IGoogleAuthService
-//     {
-//         private readonly IConfiguration _configuration;
-//         private readonly ILogger<GoogleAuthService> _logger;
-//         private SheetsService? _sheetsService;
-//         private UserCredential? _credential;
+namespace Budget_Automation.MCPServer.Services.Google
+{
+    public class GoogleAuthService(IOptions<GoogleApiSettings> settings)
+    {
+        private readonly string googleClientId = settings.Value.ClientId;
+        private readonly string googleClientSecret = settings.Value.ClientSecret;
+        private readonly string[] scopes = [SheetsService.Scope.Spreadsheets];
 
-//         public GoogleAuthService(IConfiguration configuration, ILogger<GoogleAuthService> logger)
-//         {
-//             _configuration = configuration;
-//             _logger = logger;
-//         }
+        /// <summary>
+        /// Login to Google API using OAuth2.
+        /// </summary>
+        /// <param name="googleClientId"></param>
+        /// <param name="googleClientSecret"></param>
+        /// <param name="scopes"></param>
+        /// <returns></returns>
+        public async Task<UserCredential> LoginAsync()
+        {
+            ClientSecrets secrets = new()
+            {
+                ClientId = googleClientId,
+                ClientSecret = googleClientSecret
+            };
 
-//         public async Task<SheetsService> GetSheetsServiceAsync()
-//         {
-//             if (_sheetsService != null)
-//             {
-//                 return _sheetsService;
-//             }
-
-//             var credential = await GetUserCredentialAsync();
-
-//             _sheetsService = new SheetsService(new BaseClientService.Initializer
-//             {
-//                 HttpClientInitializer = credential,
-//                 ApplicationName = _configuration["GoogleApi:ApplicationName"] ?? 
-//                     throw new InvalidOperationException("GoogleApi:ApplicationName configuration is missing")
-//             });
-
-//             return _sheetsService;
-//         }
-
-//         public async Task<UserCredential> GetUserCredentialAsync()
-//         {
-//             if (_credential != null)
-//             {
-//                 return _credential;
-//             }
-
-//             string? credentialsPath = _configuration["GoogleApi:CredentialsPath"];
-            
-//             if (string.IsNullOrEmpty(credentialsPath))
-//             {
-//                 throw new InvalidOperationException("GoogleApi:CredentialsPath configuration is missing");
-//             }
-
-//             if (!File.Exists(credentialsPath))
-//             {
-//                 _logger.LogError($"No se encontró el archivo de credenciales en: {credentialsPath}");
-//                 throw new FileNotFoundException($"No se encontró el archivo de credenciales en: {credentialsPath}");
-//             }
-
-//             using (var stream = new FileStream(credentialsPath, FileMode.Open, FileAccess.Read))
-//             {
-//                 string? tokenFolderPath = _configuration["GoogleApi:TokenFolderPath"];
-                
-//                 if (string.IsNullOrEmpty(tokenFolderPath))
-//                 {
-//                     throw new InvalidOperationException("GoogleApi:TokenFolderPath configuration is missing");
-//                 }
-
-//                 // Crear el directorio de tokens si no existe
-//                 if (!Directory.Exists(tokenFolderPath))
-//                 {
-//                     Directory.CreateDirectory(tokenFolderPath);
-//                 }
-
-//                 _credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-//                     GoogleClientSecrets.FromStream(stream).Secrets,
-//                     new[] { SheetsService.Scope.Spreadsheets, SheetsService.Scope.Drive },
-//                     "user",
-//                     CancellationToken.None,
-//                     new FileDataStore(tokenFolderPath, true)
-//                 );
-
-//                 _logger.LogInformation("Credencial guardada en: " + tokenFolderPath);
-//                 return _credential;
-//             }
-//         }
-//     }
-// }
+            return await GoogleWebAuthorizationBroker.AuthorizeAsync(secrets, scopes, "user", CancellationToken.None);
+        }
+    }
+}
